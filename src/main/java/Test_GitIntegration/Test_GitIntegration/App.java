e53ebf05778c2bdd286ff54c706ea4829d44a601
maheshbaviskar <mn.baviskar0@gail.com>
2018-04-30 16:06:27 +0530

    git ad

---
+++ src/main/java/Test_GitIntegration/Test_GitIntegration/App.java
@@ -0,0 +1,13 @@
+package Test_GitIntegration.Test_GitIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Test_GitIntegration/Test_GitIntegration/Google_Search.java
@@ -0,0 +1,44 @@
+package Test_GitIntegration.Test_GitIntegration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class Google_Search {
+	
+	private WebDriver oDriver;
+	
+	@BeforeTest
+	public void setupDriver() {
+		oDriver= new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void google_Search() {
+		
+		oDriver.get("http://www.google.com");
+		oDriver.findElement(By.name("q")).sendKeys("Salman Khan");
+		oDriver.findElement(By.name("btnK")).click();
+		WebElement wd= new WebDriverWait(oDriver, 30L).until(ExpectedConditions.presenceOfElementLocated(By.id("logocont")));
+		System.out.println(oDriver.getTitle());
+		
+	}
+	
+	@AfterTest
+	public void closeDriver()
+	{
+		oDriver.close();
+	}
+
+
+}
