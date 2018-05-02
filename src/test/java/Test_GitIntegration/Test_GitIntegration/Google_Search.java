5368fd739f1b8be7d0335bcb99f8d4643b71ef8f
maheshbaviskar <mn.baviskar0@gail.com>
2018-05-02 14:11:06 +0530

    test

--- src/test/java/Test_GitIntegration/Test_GitIntegration/Google_Search.java
+++ src/test/java/Test_GitIntegration/Test_GitIntegration/Google_Search.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+//import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
+//import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
